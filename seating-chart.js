f9a313c2ce0669c6d2de916055fefcfe7187d796
Douglas Urner <dlu@canishe.com>
2020-01-19 08:33:49 -0800

    Comments & unseated sorting.

--- seating-chart.js
+++ seating-chart.js
@@ -9,13 +9,26 @@ unseated = new Muuri('.unseated', {
     }
   },
   dragContainer: document.body,
+  // dragSortPredicate: {
+  //   action: 'swap',
+  // },
   dragSort: function() {
+    unseated.sort('name');
     return areas;
   }
 })
 // When dragging set the object dimensions to fixed
 // values so that they won't scale themselves to the
-// containing object as they move.
+// containing object as they move. This works because we are
+// adding the style property to the element (so it overrides
+// the CSS styling, then when we remove it, the CSS is still
+// there and reasserts itself).
+//
+// TODO: It appears that when you change the size of a student
+// tile as you move them between the seating area and the
+// unseated area (which can shrink the tile) Muuuri allocates
+// space for the larger size. Try fixing this by adjusting the
+// CSS in the beforeReceive() event handler.
 .on('dragStart', function(item) {
   item.getElement().style.width = 85 + 'px';
   item.getElement().style.height = item.getHeight() + 'px';
@@ -24,9 +37,14 @@ unseated = new Muuri('.unseated', {
   item.getElement().style.width = '';
   item.getElement().style.height = '';
 });
+// .on('beforeReceive', function(item) {
+//   item.getElement().style.width = '';
+//   item.getElement().style.height = '';
+// });
 
 areas.push(unseated);
 
+// Ensure an initial sort.
 unseated.sort('name', {
   layout: 'instant'
 });
@@ -36,9 +54,13 @@ allSeats.forEach(function(s) {
   var seat = new Muuri(s, {
     dragEnabled: true,
     dragContainer: document.body,
+    // dragSortPredicate: {
+    //   action: 'swap',
+    // },
     dragSort: function() {
+      unseated.sort('name');
       return areas;
-    }
+    },
   })
   areas.push(seat);
 });
